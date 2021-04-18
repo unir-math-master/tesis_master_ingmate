f80bc51ebdf7bceb0f434ca6472cafbbbf84b9d4
Erick Diaz <ewdiazsa@gail.com>
2021-04-17 22:14:48 -0600

    camera bash examples

---
+++ src/telescope-camera/display_camera.sh
@@ -0,0 +1,4 @@
+#SENSOR_ID=0 # 0 for CAM0 and 1 for CAM1 ports
+#FRAMERATE=60 # Framerate can go from 2 to 60 for 1920x1080 mode
+#gst-launch-1.0 nvarguscamerasrc sensor-id=$SENSOR_ID ! "video/x-raw(memory:NVMM),width=1920,height=1080,framerate=$FRAMERATE/1" ! nvvidconv ! nvoverlaysink
+gst-launch-1.0 nvarguscamerasrc sensor-id=0 ! "video/x-raw(memory:NVMM),width=1920,height=1080,framerate=60/1" ! nvvidconv flip-method=6 ! nvoverlaysink

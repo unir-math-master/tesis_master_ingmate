bc8497f41951aadcfff9e9951ed0127423d48d4e
Erick Diaz <ewdiazsa@gmail.com>
2021-04-29 02:19:58 -0600

    new subscriber node

---
+++ src/telescope_gps/src/gps_subscriber.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+
+def gps_callback(response):
+    rospy.logininfo(f"GPS DATA: {response.data}")
+
+
+def node():
+    rospy.init_node("gps_subscriber")
+    rospy.Subscriber('gpsdata', String, gps_callback)
+
+    rospy.spin()
+
+
+def main():
+    try:
+        node()
+    except rospy.ROSException:
+        rospy.logerr("An error occurred while trying to execute GPS")
+
+
+main()

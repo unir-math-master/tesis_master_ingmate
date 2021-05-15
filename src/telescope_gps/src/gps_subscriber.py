58ff1399caa5b04cda60cb123ecefe6d76558151
Erick Diaz <ewdiazsa@gmail.com>
2021-05-15 01:03:41 -0600

    gps sucriber node

--- src/telescope_gps/src/gps_subscriber.py
+++ src/telescope_gps/src/gps_subscriber.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
+import json
 
+
+gps_data = None
 
 def gps_callback(response):
-    rospy.loginfo(f"GPS DATA: {response.data}")
+    raw_repsonse = response.data
+    if raw_repsonse != 'null':
+        print(type(response.data))
+        payload_data = json.loads(raw_repsonse)
+        with open("gui/gps_payload.json", 'a', encoding='utf-8') as f:
+            f.write(raw_repsonse+"\n")
+
+        rospy.loginfo(f"GPS DATA: {payload_data}")
 
 
 def node():
